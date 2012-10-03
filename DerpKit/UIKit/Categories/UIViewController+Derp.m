9bd369cce5c6a819a9561e40158e3026b828cd2d
Yang Meyer <mail@yangmeyer.de>
2012-10-03 13:32:48 +0200

    Unverbosify - no NSLog output

--- DerpKit/UIKit/Categories/UIViewController+Derp.m
+++ DerpKit/UIKit/Categories/UIViewController+Derp.m
@@ -25,7 +25,6 @@
 	NSOperationQueue *mainQueue = [NSOperationQueue mainQueue];
 	id willShow = [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillShowNotification object:nil queue:mainQueue usingBlock:^(NSNotification *note) {
 		[self derp_performIfVisible:^{
-			NSLog(@"notif: %@",[note userInfo]);
 			CGRect keyboardFrame = [self.view convertRect:[(NSValue *)note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue]
 												 fromView:nil];
 			CGRect viewFrame = self.view.frame;
@@ -44,7 +43,6 @@
 	
 	id willHide = [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillHideNotification object:nil queue:mainQueue usingBlock:^(NSNotification *note) {
 		[self derp_performIfVisible:^{
-			NSLog(@"notif: %@",[note userInfo]);
 			CGRect keyboardFrame = [(NSValue *)note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
 			CGRect viewFrame = self.view.frame;
 			viewFrame.size.height += keyboardFrame.size.height;
